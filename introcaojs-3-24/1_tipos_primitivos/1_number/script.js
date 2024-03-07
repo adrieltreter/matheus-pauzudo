dacd095aab481fcc360536263709580e04a6be62
@adriel_abner397 <126088211+adrieltreter@users.noreply.github.com>
2024-03-06 21:00:19 -0300

    Add files via upload

---
+++ introcaojs-3-24/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,11 @@
+//tipos number
+
+const meunumero = 24;
+
+const telefone = 4499779478;
+
+const idade = 29;
+
+console.log(meunumero);
+console.log(telefone);
+console.log(idade);
---
+++ introcaojs-3-24/script.js
@@ -0,0 +1,10 @@
+const texto1 = "matheus gostoso"
+const texto2 = "matheus safadinho"
+
+const texto3 = "pauzudo"
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
+
+    console.log(texto2, texto3)
